aae2a880d589364899c01a005dc5005a242c06be
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-24 18:37:04 +1000

    add camera function

--- App.js
+++ App.js
@@ -1,46 +1,50 @@
 import React, {useEffect, useState} from 'react';
-import { StyleSheet, View, Alert, Dimensions} from 'react-native';
-import MapView, {Marker} from 'react-native-maps';
-import * as Location from 'expo-location';
-import Constants from 'expo-constants';
+import { StyleSheet, View, Text, Button} from 'react-native';
+import { Camera } from 'expo-camera';
+
 
 export default function App() {
-  const [location, setLocation] = useState({});
-  const buscarLocation = async () => {
-    let {status} = await Location.requestPermissionsAsync();
-    if(status !== 'granted') {
-      Alert.alert('Warning','Permission to access location was denied')
-    }
-      let location = await Location.getCurrentPositionAsync({});
-    setLocation(location);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const getPermission = async () => {
+    let { status } = await Camera.requestPermissionsAsync();
+    setHasPermission(status == 'granted')
+    console.log(status);
   };
   useEffect( () => {
-    buscarLocation()
+    getPermission();
   })
+
+  if(hasPermission === null) {
+    return <View><Text>Esperando permisos</Text></View>
+  }
+  if(hasPermission === false) {
+    return <View><Text>No tenemos acceso a la camara</Text></View>
+  }
     return (
       <View style={styles.container}>
-       <MapView style={styles.map}>
-         {location.coords 
-          ? <Marker
-          coordinate= {location.coords}
-          title= 'Marcado'
-          description='Punto actual'
-          >
-          </Marker>
-          : null
-        }
-         
-       </MapView>
+        <Camera 
+        style= {styles.camera}
+        type={type}
+        >
+          <Button
+          title='Cambiar Camara'
+          onPress={() => {
+            const {front, back} = Camera.Constants.Type;
+            const newType = type === back ? front : back;
+            setType(newType)
+          }}
+          ></Button>
+        </Camera>
       </View>
     );
 }
 
-const styles = StyleSheet.create({
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-
-  },  
+const styles = StyleSheet.create({  
+  camera: {
+    flex: 1,
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
