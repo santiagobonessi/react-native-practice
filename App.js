631adba826c8744ce3f297c5f115f9a7afb26772
Santiago Bonessi <santiagobonessi@hotmail.com>
2020-05-24 17:41:53 +1000

    add location map function

--- App.js
+++ App.js
@@ -1,41 +1,51 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TouchableNativeFeedback, TextInput } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import { StyleSheet, View, Alert, Dimensions} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
+import Constants from 'expo-constants';
 
 export default function App() {
-  const [text, setText] = useState('Hello World');
-  const [submit, setSubmit] = useState('');
-  return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder= 'Escribe aqui'
-        onChangeText={t => setText(t)}
-        defaultValue={text}
-      />
-      <TouchableNativeFeedback
-        background={TouchableNativeFeedback.Ripple('#00F')}
-        onPress={() => {
-          setSubmit(text)
-          alert('Texto enviado con exito.')
-        }}>
-          <Text>ACEPTAR</Text>
-      </TouchableNativeFeedback>
-    </View>
-  );
+  const [location, setLocation] = useState({});
+  const buscarLocation = async () => {
+    let {status} = await Location.requestPermissionsAsync();
+    if(status !== 'granted') {
+      Alert.alert('Warning','Permission to access location was denied')
+    }
+      let location = await Location.getCurrentPositionAsync({});
+    setLocation(location);
+  };
+  useEffect( () => {
+    buscarLocation()
+  })
+    return (
+      <View style={styles.container}>
+       <MapView style={styles.map}>
+         {location.coords 
+          ? <Marker
+          coordinate= {location.coords}
+          title= 'Marcado'
+          description='Punto actual'
+          >
+          </Marker>
+          : null
+        }
+         
+       </MapView>
+      </View>
+    );
 }
 
 const styles = StyleSheet.create({
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+
+  },  
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'space-evenly',
+    paddingTop: 22,
   },
-  text: {
-    color: 'white',
-    fontSize: 24,
-  },
-  input: {
-
-  }
 });
